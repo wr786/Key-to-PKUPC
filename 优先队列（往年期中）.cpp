6a58bbd801c7b6081ad2e93fc4c5b630f18f14b7
wr786 <wr786@users.noreply.github.com>
2019-10-28 13:38:45 +0800

    Updated a new solution

---
+++ 优先队列（往年期中）.cpp
@@ -0,0 +1,41 @@
+#include<cstdio>
+int heap[200001];
+int n,m,tmp,Min=9999999,Max=0;
+char op[21];
+
+void read_in()
+{
+	scanf("%d",&tmp);
+	heap[tmp]++;
+	if(tmp>Max) Max=tmp;
+	if(tmp<Min) Min=tmp;
+}
+
+int main()
+{
+
+	scanf("%d",&n);
+	while(n--) read_in();
+	scanf("%d",&m);
+	while(m--)
+	{
+		scanf("%s",op);
+		if(op[0]=='A')
+			read_in();
+		else
+		{
+			int flag=1;
+			for(int i=Min;i<=Max;i++)
+				if(heap[i])
+				{
+					printf("%d\n",i);
+					heap[i]--;
+					flag=0;
+          Min=i;
+          break;
+				}
+				if(flag) printf("NULL\n");
+		}
+	}
+	return 0;
+}
