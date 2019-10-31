04ed44138366b640769fdf53cb54ddc8ec30854c
wr786 <wr786@users.noreply.github.com>
2019-10-31 22:10:47 +0800

    Updated a new solution to 试液稀释

--- 试液稀释（2017期中）.cpp
+++ 试液稀释（2017期中）.cpp
@@ -1,3 +1,31 @@
+// 线性DP：最长下降子序列
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<string>
+using namespace std;
+double a[101];
+int d[101];
+
+int main() {
+	int n; cin >> n;
+	for(int i=1; i<=n; i++)
+		cin >> a[i];
+	// 最长下降子序列
+	int ret = 1;
+	for(int i=1; i<n; i++) {
+		d[i] = max(d[i], 1); // 初始化
+		for(int j=i+1; j<=n; j++)
+			if(a[j] <= a[i]) {
+				d[j] = max(d[j], d[i]+1); // 状态转移方程
+				ret = max(ret, d[j]);
+			}
+	}
+	cout << ret << endl;
+	return 0;
+}
+
+// 如果看不懂DP的话可以尝试这种暴力的搜索方法
 #include<iostream>
 #include<cstdio>
 #include<algorithm>
